3a37f1aef041b90776a12eb80fcb7ffe9d2771fd
Andre Zammit <mawzer@gmail.com>
2015-12-13 13:20:43 +0100

    Spacing.

--- background.js
+++ background.js
@@ -21,9 +21,9 @@ chrome.app.runtime.onLaunched.addListener(
   		{
     		"bounds": 
     		{
-                "width": Math.round(window.screen.availWidth*0.8),
-                "height": Math.round(window.screen.availHeight*0.8)
-            }   
+                "width": Math.round(window.screen.availWidth * 0.8),
+                "height": Math.round(window.screen.availHeight * 0.8)
+            }
   		});
 });
 
