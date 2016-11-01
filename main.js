e1b596ad96ebf4b2b47136012bbbaa9e60e4c011
Andre Zammit <mawzer@gmail.com>
2016-11-01 21:35:36 +0100

    Added debug parameter.

--- main.js
+++ main.js
@@ -4,40 +4,58 @@ const {app, BrowserWindow} = require('electron');
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
-function createWindow () 
+function findArg(element)
 {
-  // Create the browser window.
-  win = new BrowserWindow(
-    {
-      show: false,
-      title: 'Recipe Manager',
-      width: 1300, 
-      height: 800,
-      minWidth: 1150,
-      minHeight: 780,
-      icon: 'icons/icon-128.png'
-    });
+	console.log(this);
+	console.log(element);
 
-  //win.setMenu(null);
+	return element === this.toString();
+}
 
-  // and load the index.html of the app.
-  win.loadURL(`file://${__dirname}/index.html`, { });
+function createWindow() 
+{
+	var isDebug = false;
 
-  win.once('ready-to-show', () => 
-  {
-    win.show();
-  });
+	if (process.argv.find(findArg, "--debug"))
+		isDebug = true;
 
-  // Open the DevTools.
-  win.webContents.openDevTools();
+	// Create the browser window.
+	win = new BrowserWindow(
+		{
+			show: false,
+			title: 'Recipe Manager',
+			width: 1300,
+			height: 800,
+			minWidth: 1150,
+			minHeight: 780,
+			icon: 'icons/icon-128.png'
+		});
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
+	if (isDebug)
+	{
+		win.webContents.openDevTools();
+	}
+	else
+	{
+		win.setMenu(null);
+	}
+
+	// and load the index.html of the app.
+	win.loadURL(`file://${__dirname}/index.html`, {});
+
+	win.once('ready-to-show', () => 
+	{
+		win.show();
+	});
+
+	// Emitted when the window is closed.
+	win.on('closed', () =>
+	{
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		win = null;
+	});
 }
 
 // This method will be called when Electron has finished
@@ -46,20 +64,24 @@ function createWindow ()
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', () =>
+{
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin')
+	{
+		app.quit();
+	}
 });
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow();
-  }
+app.on('activate', () =>
+{
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (win === null)
+	{
+		createWindow();
+	}
 });
 
 // In this file you can include the rest of your app's specific main process
