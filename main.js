5f7fcbada9a3c6df58797b8c41f25bb19c2d72a4
Andre Zammit <mawzer@gmail.com>
2016-11-12 01:08:27 +0100

    Setting custom application menu.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -27,6 +27,34 @@ function createWindow()
 			minHeight: 780,
 			icon: 'icons/icon.ico'
 		});
+
+	var template =
+		[
+			{
+				label: "Application",
+				submenu:
+				[
+					{ label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+					{ type: "separator" },
+					{ label: "Quit", accelerator: "Command+Q", click: function () { app.quit(); } }
+				]
+			},
+			{
+				label: "Edit",
+				submenu:
+				[
+					{ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+					{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+					{ type: "separator" },
+					{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+					{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+					{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+					{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+				]
+			}
+		];
+
+	Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 
 	if (isDebug)
 	{
