f38d2f97223eea466c6bb907a79b608b4cd30167
André Zammit <mawzer@gmail.com>
2026-01-01 22:52:34 +0100

    Caching Google auth token.

--- modules/OAuth.js
+++ modules/OAuth.js
@@ -4,40 +4,95 @@ const OAuth = (function ()
     const _clientId = "13277472194-hs9hse9r1oc5tkr65si7uma916r1nenu.apps.googleusercontent.com";
 
     const _scopes = "https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/calendar";
-    const _discoveryDocs = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest", "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
-
-    let tokenClient;
+    
+    const _discoveryDocs = [
+        "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest", 
+        "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"
+    ];
 
     function initClient(callback)
     {
         const authObj = {
-            discoveryDocs: _discoveryDocs,
+            discoveryDocs: _discoveryDocs
         };
 
         gapi.client.init(authObj).then(
-            function () {
-                tokenClient = google.accounts.oauth2.initTokenClient({
-                    scope: _scopes,
-                    client_id: _clientId,
-                    callback: onSignedInUpdated
-                });
+            googleSignIn, 
+            onGoogleSignInError);
 
-                if (gapi.client.getToken() === null) {
-                    tokenClient.requestAccessToken({prompt: 'consent'});
-                } else {
-                    tokenClient.requestAccessToken({prompt: ''});
-                }
-            }, function (error) {
-                callback(error);
+        function googleSignIn() {
+            const tokenClient = google.accounts.oauth2.initTokenClient({
+                scope: _scopes,
+                client_id: _clientId,
+                callback: onSignedInUpdated
             });
 
-        async function onSignedInUpdated(resp) {
-            if (resp.error !== undefined) {
-                throw (resp);
+            try {
+                const storedAccessToken = getValidAccessToken();
+
+                if (storedAccessToken) {
+                    gapi.client.setToken({
+                        access_token: storedAccessToken
+                    });
+
+                    callback(null);
+                    return;
+                }
+            } catch (exception) {
+                // fall through
+            }
+
+            tokenClient.requestAccessToken({ prompt: '' });
+        }
+
+        function onGoogleSignInError(error) {
+            callback(error);
+        }
+
+        async function onSignedInUpdated(authResponse) {
+            if (authResponse.error !== undefined) {
+                throw (authResponse);
+            }
+
+            // resp contains access_token and expires_in (seconds)
+            try {
+                if (authResponse.access_token) {
+                    saveAccessToken(authResponse);
+
+                    gapi.client.setToken({ 
+                        access_token: authResponse.access_token 
+                    });
+                }
+            } catch (exception) {
+                console.error('Error storing access token: ', exception);
             }
 
             callback(null);
         }
+    }
+
+    function getValidAccessToken()
+    {
+        const storedToken = JSON.parse(localStorage.getItem('gm_oauth_token') || 'null');
+                
+        if (storedToken && storedToken.access_token && storedToken.expires_at && Date.now() < storedToken.expires_at) {
+            return storedToken.access_token;
+        }
+
+        return null;
+    }
+
+    function saveAccessToken(authResponse)
+    {
+        const expiresIn = authResponse.expires_in || 0;
+        const expiresAt = Date.now() + (expiresIn * 1000);
+        
+        const storedAccessToken = {
+            access_token: authResponse.access_token,
+            expires_at: expiresAt
+        };
+
+        localStorage.setItem('gm_oauth_token', JSON.stringify(storedAccessToken));
     }
 
     function authenticate(callback)
